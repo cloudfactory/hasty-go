dba90bff10f91518b36275dbcd74984780bc4fbe
Andriy Borodiychuk <ab@markusweb.com>
2020-10-26 20:54:46 +0100

    Fix params in helper

--- helpers.go
+++ helpers.go
@@ -10,4 +10,4 @@ func Bool(v bool) *bool { return &v }
 func Int(v int) *int { return &v }
 
 // Float64 helper allows to quickly get a pointer to a float46
-func Float64(v int) *int { return &v }
+func Float64(v float64) *float64 { return &v }
